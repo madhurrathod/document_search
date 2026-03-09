3721247a7c10e2b3d399e1a9cecb2f5d56cbd60e
Madhur Rathod <madhur.thanku@gmail.com>
2026-03-10 00:50:56 +0530

    Read pdf
    implemented functionality to read pdf using pypdf library and printed the first page of pdf Attention is all you need

---
+++ src/script.py
@@ -0,0 +1,11 @@
+from pypdf import PdfReader
+
+reader = PdfReader("temporary_docs/Attention Is All You Need.pdf")
+
+number_of_pages = len(reader.pages)
+pageObj = reader.pages[0]
+
+content = pageObj.extract_text()
+
+print(number_of_pages)
+print(content)
