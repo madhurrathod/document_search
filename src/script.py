9daed49ce70a7b23ee1aa4b72df202a2b82c3075
Madhur Rathod <madhur.thanku@gmail.com>
2026-03-15 13:18:42 +0530

    fix(ReadDoc class): implement error handling

--- src/script.py
+++ src/script.py
@@ -2,20 +2,23 @@ from pypdf import PdfReader
 class ReadDoc:
     def __init__(self, url):
         self.url = url
-        self.reader = PdfReader(url)
+        try:
+            self.reader = PdfReader(url)
+        except:
+            print("Incorrect URL")
     
     def getPageContent(self, pageNo):
         try:
             pageObj = self.reader.pages[pageNo]
         except:
-            print("Page not Found")
+            return "Page Not Found"
         else:
             content = pageObj.extract_text()
             return content
         
-url = "temporary_docs/Attention Is All You Need.pdf"
+url = "temporary_docs/Attention Is Allou Need.pdf"
 doc1 = ReadDoc(url)
-print(doc1.getPageContent(0))
+print(doc1.getPageContent(100))
         
         
         
