1b421021410079f943e3932c7a1d530db96c5bf8
Madhur Rathod <madhur.thanku@gmail.com>
2026-03-17 14:51:30 +0530

    chore: add .gitignore file
    just noticed that there's __pycache__ folder which was unnecessary still was on github so ignoring it now

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,10 @@
 # Main purpose of this file is to convert the PDF file's content that we'll upload, in the format where all the sentences of that file gets a label of name of that file.
 
-import script
+import read
 
-doc1 = script.ReadDoc("temporary_docs/Attention Is All You Need.pdf")
-print(doc1.getPageContent(9))
+doc1 = read.ReadDoc("temporary_docs/Attention Is All You Need.pdf")
+size = doc1.pageCount()
+onePage = doc1.getPageContent(0)
+onePageSplit = onePage.split(".")
+print(onePageSplit)
+
