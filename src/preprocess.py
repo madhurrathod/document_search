21b9c9bbd39adddb89c323a4c02f2cfc6ec14ebc
Madhur Rathod <madhur.thanku@gmail.com>
2026-03-19 22:16:17 +0530

    feat: create numpy array after reading text from resource

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,11 +1,14 @@
 # Main purpose of this file is to convert the PDF file's content that we'll upload, in the format where all the sentences of that file gets a label of name of that file.
 
 import read
+import numpy as np
 
-doc1 = read.ReadDoc("temporary_docs/Attention Is All You Need.pdf")
-size = doc1.pageCount()
-onePage = doc1.getPageContent(0)
-onePageSplit = onePage.split(".")
-print(onePageSplit)
+doc = read.ReadDoc("temporary_docs/Attention Is All You Need.pdf")
+size = doc.pageCount()
 
-
+wholeArr = np.array([])
+Page = doc.getPageContent(0)
+removeSlashN = Page.replace("\n", " ")
+splitSentences = removeSlashN.split(".")
+nparr = np.array(splitSentences)
+print(nparr)
