036a9566ce7b56609a5b18e632378905982f7e0a
Madhur Rathod <madhur.thanku@gmail.com>
2026-03-30 23:47:40 +0530

    feat: get all sentences from doc and store in a variable

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,20 +2,23 @@
 
 import read
 import numpy as np
+import pandas as pd
 
 doc = read.ReadDoc("temporary_docs/Attention Is All You Need.pdf")
-size = doc.pageCount()
+total_pages = doc.pageCount()
 
-# wholeArr = np.array([])
-Page = doc.getPageContent(0)
-removeSlashN = Page.replace("\n", " ")
-splitSentences = removeSlashN.split(".")
-# nparr = np.array(splitSentences)
-# print(nparr)
+def cleanPage(page):
+    removeSlashN = page.replace("\n", " ")
+    splitSentences = removeSlashN.split(".")
+    return splitSentences
 
+All_sentences = []
+for i in range(total_pages):
+    page = doc.getPageContent(i)
+    sentences = cleanPage(page)
+    All_sentences = All_sentences + sentences
 
-doc_label = "Attention Is All You Need"
-import pandas as pd
-df = pd.DataFrame(splitSentences)
-label = pd.array(data = doc_label,dtype="string" )
+print(len(All_sentences))
+    
+
 
