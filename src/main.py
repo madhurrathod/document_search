b295a43d32cdd8acc2346bd723c9e28b97c41652
Madhur Rathod <madhur.thanku@gmail.com>
2026-04-18 21:27:50 +0530

    feat(main.py): createing csv dataset only when specified
    ususally it would've make the dataset.csv on every time the app reloads which could've made the app very unoptimized

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
+import sys
 from models.tf_idf import TfidfSearch
 from preprocess import PDFPreprocessor
 
-processor = PDFPreprocessor("temporary_docs")
-processor.run()
+if "--rebuild" in sys.argv:
+    processor = PDFPreprocessor("temporary_docs")
+    processor.run()
+    print("Dataset rebuilt!")
 
 search_engine = TfidfSearch("dataset.csv")
-print(search_engine.search("attention mechanism"))
+print(search_engine.search("human interaction with llm"))
