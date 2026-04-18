e15db91d806ebbe2c26ea266bd740a30953e0530
Madhur Rathod <madhur.thanku@gmail.com>
2026-04-18 21:53:52 +0530

    feat(main): basic frontend implementation for document search

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 import streamlit as st
 import pandas as pd
 from models.tf_idf import TfidfSearch
@@ -6,10 +6,24 @@ from preprocess import PDFPreprocessor
 
 st.title("Document Search")
 
-if "--rebuild" in sys.argv:
+if st.button("Rebuild Dataset"):
     processor = PDFPreprocessor("temporary_docs")
     processor.run()
-    print("Dataset rebuilt!")
+    st.success("Dataset rebuilt!")
 
+st.subheader("All documents")
+data = pd.read_csv("dataset.csv")
+docs = [f.replace(".pdf","") for f in os.listdir("temporary_docs")]
+for d in docs:
+    st.write(d)
+    
 search_engine = TfidfSearch("dataset.csv")
-print(search_engine.search("human interaction with llm"))
+query = st.text_input("Give me the context")
+
+if st.button("Search"):
+    if query:
+        results = search_engine.search(query)
+        
+        st.subheader("Matching Documents:")
+        for label,score in results.items():
+            st.write(label, "->", round(score,4))
